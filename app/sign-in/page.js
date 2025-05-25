e7f25564b54e8566af3818e62006c41d8a789cfb
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-25 23:34:58 +0530

    Create page.js

---
+++ app/sign-in/page.js
@@ -0,0 +1,14 @@
+'use client';
+
+import { SignIn } from '@clerk/nextjs';
+
+export default function SignInPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <SignIn
+        forceRedirectUrl="/other"
+        signUpForceRedirectUrl="/other"
+      />
+    </div>
+  );
+}
