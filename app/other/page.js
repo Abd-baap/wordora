547ff9ff516b80ffe93470871df0255b241e0964
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-25 23:19:45 +0530

    Update page.js

--- app/other/page.js
+++ app/other/page.js
@@ -1,18 +1,20 @@
 'use client';
-import {SignedIn,SignIn ,SignedOut } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import Manager from '../components/manager';
 
 export default function HighlightText() {
+const { userId } = auth();
 
+  if (!userId) {
+    redirect('/sign-in'); // or a custom URL
+  }
   return (
     <div className="min-h-screen w-[100%] flex justify-center items-center flex-col">
-   <SignedOut>
-    <SignIn/>
-    </SignedOut>
-    <SignedIn>
+   
     <Manager />
-    </SignedIn>
+    
   </div>
     );
 }
