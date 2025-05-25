b868e1cc81639ec5d77f1c316d08523bab680a72
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-25 22:41:35 +0530

    Update page.js

--- app/other/page.js
+++ app/other/page.js
@@ -1,5 +1,5 @@
 'use client';
-
+import {SignedIn,SignIn ,SignedOut } from '@clerk/nextjs'
 import { useState, useEffect } from 'react';
 import Manager from '../components/manager';
 
@@ -7,7 +7,12 @@ export default function HighlightText() {
 
   return (
     <div className="min-h-screen w-[100%] flex justify-center items-center flex-col">
-   <Manager />
+   <SignedOut>
+    </SignIn>
+    </SignedOut>
+    <SignedIn>
+    <Manager />
+    </SignedIn>
   </div>
     );
 }
